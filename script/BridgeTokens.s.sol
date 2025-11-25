94c6889deb3baee86669f25f3dbd986065991652
unknown <muhammedko420@gmail.com>
2025-11-25 14:30:27 +0100

    onchain activities

--- script/BridgeTokens.s.sol
+++ script/BridgeTokens.s.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {IRouterClient} from "@ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
+
+// Script to bridge tokens via CCIP and send CCIP messages
 
 contract BridgeTokensScript is Script {
     function run(
@@ -26,11 +30,14 @@ contract BridgeTokensScript is Script {
             data: "",
             tokenAmounts: tokenAmounts,
             feeToken: linkTokenAddress,
-            extraArgs: Client.EVM2AnyMessageExtraArgsV1({
-                gasLimit: 200000,
-                feeTokenAddress: address(0)
-            })
+            extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({gasLimit: 0}));
         });
+        uint256 ccipFee = IRouterClient(routerAddress).getFee(
+            destinationChainSelector,
+            message
+        );
+        IERC20(linkTokenAddress).approve(routerAddress, ccipFee);
+        IERC20(tokenToSendAddress).approve(routerAddress, amountToSend);
         IRouterClient(routerAddress).ccipSend(
             destinationChainSelector,
             message
