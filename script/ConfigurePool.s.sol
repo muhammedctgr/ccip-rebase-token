ed08cd57716fb0b3da3c98b325b68a61b8280aa3
unknown <muhammedko420@gmail.com>
2025-11-25 12:07:09 +0100

    onchain activities

---
+++ script/ConfigurePool.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+
+contract ConfigurePoolScript is Script {
+    function setup(address localPool, uint64 remoteChainSelector, address remotePool, address remoteToken, bool)
+}
